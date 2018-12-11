6e37119e9f5435e4f3a60e9fb5b0d59f36dea30c
Tudor <hututudor@yahoo.ro>
2018-12-11 18:39:52 +0200

    first commit

---
+++ precache-manifest.65bd74d3e6e383fbf58b7e6579decb04.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b62fc447ffcae800c54",
+    "url": "/static/js/main.8b62fc44.chunk.js"
+  },
+  {
+    "revision": "75a3919b72a9555f7cee",
+    "url": "/static/js/1.75a3919b.chunk.js"
+  },
+  {
+    "revision": "03cfaddae99d7a892b653781e3d1d715",
+    "url": "/index.html"
+  }
+];
